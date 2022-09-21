e704156506868297ac72ad61f10e079dfc936fe6
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-21 18:06:00 -0500

    initial commit

---
+++ ClassesExercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClassesExercise1;
+
+
+var car1 = new Car();
+car1.Make = "Ford";
+car1.Model = "Escape";
+car1.Year = 2000;
+
+Console.WriteLine($"My car is a {car1.Year} {car1.Make} {car1.Model}.  I wish it were newer!");
+
+
+var car2 = new Car();
+car2.Make = "Tesla";
+car2.Model = "Model S";
+car2.Year = 2022;
+
+Console.WriteLine($"Wow! What a fancy looking car you have.  Is that the {car2.Year} {car2.Model} {car2.Make}?!");
+
+
+
+
+
+
+
+
+    
